24ec1ae6be1290c064c182709bd1571933838ebd
ToKa <st155019@stud.uni-stuttgart.de>
2024-05-27 06:19:28 +0200

    working version with chat and building design

---
+++ ionic_app/photo-gallery/src/app/chat/chat.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { OpenaiService } from '../openai.service';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.scss'],
+})
+export class ChatComponent {
+  messages: { text: string, isResponse?: boolean }[] = []; // Array to store chat messages with isResponse flag
+  newMessage: string = ''; // Variable to store new message
+
+  constructor(private openaiService: OpenaiService) { }
+
+  // Function to send a message
+  sendMessage() {
+    let trimmed_msg = this.newMessage.trim();
+    if (trimmed_msg !== '' && (this.messages.length == 0 || this.messages[this.messages.length-1].text !== 'typing ...')) {
+      this.newMessage = ''; // Clear input field after sending message
+      this.messages.push({ text: trimmed_msg });
+      this.messages.push({ text: "typing ...", isResponse: true });
+      this.openaiService.chatGPT(trimmed_msg).then((response) => {
+        var responseMessage: string = "";
+        if (response.message.content != undefined && response.message.content != null) {
+          responseMessage = response.message.content;
+        }
+        this.messages.pop();
+        this.messages.push({ text: responseMessage, isResponse: true });
+      }).catch(error => {
+        console.error('error:', error);
+      });
+    }
+  }
+}
--- ionic_app/photo-gallery/src/main.ts
+++ ionic_app/photo-gallery/src/main.ts
@@ -10,3 +10,10 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+// async function main() {
+//   const openaiService = new OpenaiService(); // Instantiate OpenaiService
+//   const message = "Give me 3 names"; // Specify your message
+//   const completion = await openaiService.chatGPT(message); // Call chatGPT method
+//   console.log(completion);
+// }
