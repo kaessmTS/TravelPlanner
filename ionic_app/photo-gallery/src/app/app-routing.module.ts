5265757115c074f0721c191a1f82cc53b6138d29
ToKa <st155019@stud.uni-stuttgart.de>
2024-06-02 09:24:50 +0200

    dallee image generation

--- ionic_app/photo-gallery/src/app/app-routing.module.ts
+++ ionic_app/photo-gallery/src/app/app-routing.module.ts
@@ -4,9 +4,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
- },
-  {
-    path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   }
 ];
